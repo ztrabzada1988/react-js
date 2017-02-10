b3a910b2b72388d7e18f91c30f685d8feda31a5d
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-09 19:39:17 -0800

    initial commit

---
+++ person-list.js
@@ -0,0 +1,20 @@
+var React = require('react');
+
+var Person = require('./person');
+
+var PersonList = React.createClass({
+    render: function () {
+        var people = [];
+        for (var i = 0; i < 5; i++) {
+            people.push( < Person / > );
+        }
+        return (
+            <div className="person-list">
+                {people}
+            </div>
+        );
+    }
+});
+
+
+module.exports = PersonList;
---
+++ person.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Person = function(props) {
+    //var name = 'Derek Zoolander';
+    //var imageUrl = 'http://mcgrelio.com/gallery/var/albums/vacanze/capodanno2004/capodanno2004_bluesteel/derek.jpg?m=1286923065';
+    //var job = 'Male model';
+    return (
+        <div className="person">
+            <div className="person-name">{props.name}</div>
+            <img className="person-img" src={props.imageUrl} />
+            <div className="person-job">
+                {props.job}
+            </div>
+        </div>
+    );
+};
+
+module.exports = Person;
