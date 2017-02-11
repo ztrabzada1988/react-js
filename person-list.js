ae77bf95ca7b334e277a77c0979b749e5ba6b063
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-10 16:17:52 -0800

    adding stateful components

--- person-list.js
+++ person-list.js
@@ -2,19 +2,35 @@ var React = require('react');
 
 var Person = require('./person');
 
-var PersonList = React.createClass({
-    render: function () {
-        var people = [];
-        for (var i = 0; i < 5; i++) {
-            people.push( < Person / > );
-        }
-        return (
-            <div className="person-list">
-                {people}
-            </div>
-        );
-    }
-});
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person name="Derek Zoolander"
+                    imageUrl="http://mcgrelio.com/gallery/var/albums/vacanze/capodanno2004/capodanno2004_bluesteel/derek.jpg?m=1286923065"
+                    job="Male Model" />
+            <Person name="Donald Knuth"
+                    imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+                    job="Clever Chap" />
+        </div>
+    );
+};
+
+
+
+
+//var PersonList = React.createClass({
+//    render: function () {
+//        var people = [];
+//        for (var i = 0; i < 5; i++) {
+//            people.push( < Person / > );
+//        }
+//        return (
+//            <div className="person-list">
+//                {people}
+//            </div>
+//        );
+//    }
+//});
 
 
 module.exports = PersonList;
--- person.js
+++ person.js
@@ -1,19 +1,44 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function(props) {
+//var Person = function(props) {
     //var name = 'Derek Zoolander';
     //var imageUrl = 'http://mcgrelio.com/gallery/var/albums/vacanze/capodanno2004/capodanno2004_bluesteel/derek.jpg?m=1286923065';
     //var job = 'Male model';
-    return (
-        <div className="person">
-            <div className="person-name">{props.name}</div>
-            <img className="person-img" src={props.imageUrl} />
+  //  return (
+        //<div className="person">
+          //  <div className="person-name">{props.name}</div>
+          //  <img className="person-img" src={props.imageUrl} />
+          //  <div className="person-job">
+                //{props.job}
+          //  </div>
+       // </div>
+   // );
+//};
+ // });
+var Person = React.createClass({
+    getInitialState: function() {
+        return {
+            highlight: false
+        };
+    },
+    onClick: function() {
+        this.setState({
+            highlight: !this.state.highlight
+        });
+    },
+    render: function() {
+        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+        return (
+            <div className={classes} onClick={this.onClick}>
+            <div className="person-name">{this.props.name}</div>
+            <img className="person-img" src={this.props.imageUrl} />
             <div className="person-job">
-                {props.job}
+            {this.props.job}
             </div>
-        </div>
-    );
-};
+            </div>
+        );
+    }
+});
 
 module.exports = Person;
