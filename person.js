f7cbc7c43fe77e6f975a7f70d404c583aa150520
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-10 16:20:51 -0800

    adding stateful components

--- person.js
+++ person.js
@@ -30,7 +30,7 @@ var Person = React.createClass({
         });
     },
     render: function() {
-        // if highlight is
+        // if highlight is true, add highlight
         var classes = 'person' + (this.state.highlight ? 'highlight' : '');
         return (
             <div className={classes} onClick={this.onClick}>
